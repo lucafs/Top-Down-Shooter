a88c80ed0b6a2abd37bf4a4f30860ee964b41c88
gubenites <gubenites99@gmail.com>
2021-04-04 20:13:39 -0300

    atualizado

---
+++ Top down shooter/Assets/_Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    [Range(1, 10)]
+    public float velocidade;
+
+    public int vida = 3;
+
+    private void Start()
+    {
+     
+    }
+
+    void Update()
+    {
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+        transform.position += new Vector3(inputX, inputY, 0) * Time.deltaTime * velocidade;
+    }
+}
