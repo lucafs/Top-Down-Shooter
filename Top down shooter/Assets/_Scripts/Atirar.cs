aff8fdb6ea55af4fe1841a9ee2861710a4287004
luca farah <luca@ambientedecriacao.com.br>
2021-04-05 12:26:36 -0300

    player + shooting +- map

---
+++ Top down shooter/Assets/_Scripts/Atirar.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Atirar : MonoBehaviour
+{
+    public Transform arma;
+    public GameObject tiroPrefab;
+
+    public float bulletForce = 20f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1")){
+            Shoot();
+        }
+        
+    }
+    void Shoot(){
+        GameObject tiro = Instantiate(tiroPrefab , arma.position,arma.rotation);
+        Rigidbody2D rb= tiro.GetComponent<Rigidbody2D>();
+        rb.AddForce(-arma.up * bulletForce, ForceMode2D.Impulse);
+
+
+    }
+}
---
+++ Top down shooter/Assets/_Scripts/MovimentoPlayer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimentoPlayer : MonoBehaviour
+{
+    public float velocidade = 5f;
+    public Rigidbody2D rb;
+    Vector2 movimento;
+    Vector2 mousePos;
+    Vector2 lookDir;
+    // public Camera cam; 
+
+    // Update is called once per frame
+    void Update()
+    {
+        movimento.x = Input.GetAxisRaw("Horizontal");
+        movimento.y = Input.GetAxisRaw("Vertical");
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        
+    }
+    void FixedUpdate(){
+        rb.MovePosition(rb.position + movimento * velocidade * Time.fixedDeltaTime);
+        lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y,lookDir.x)* Mathf.Rad2Deg ;
+        rb.rotation = angle;
+    }
+}
