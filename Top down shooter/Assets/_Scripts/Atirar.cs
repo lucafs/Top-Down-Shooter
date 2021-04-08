5044a31553f58cb09a9b96f8c1355141683504aa
gubenites <gubenites99@gmail.com>
2021-04-08 15:25:05 -0300

    feito tudo

--- Top down shooter/Assets/_Scripts/Atirar.cs
+++ Top down shooter/Assets/_Scripts/Atirar.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Atirar : MonoBehaviour
 {
     GameManager gm;
-
+    public AudioClip shootSFX;
     public Transform arma;
     public GameObject tiroPrefab;
 
@@ -29,21 +29,20 @@ public class Atirar : MonoBehaviour
 
         int reloadingValue = animator.GetInteger("Reloading");
 
-        if(counter > 2 && reloadingValue == 1)
+        if(counter > 1 && reloadingValue == 1)
         {
             animator.SetInteger("Reloading", 0);
         }
 
         counter += Time.deltaTime;
 
-        if (Input.GetButtonDown("Fire1") && Bullets > 0){
+        if (Input.GetButtonDown("Fire1") && Bullets > 0 && animator.GetInteger("Reloading") != 1){
             animator.SetInteger("Reloading", 0);
             Bullets -= 1;
             Shoot();
-            StartCoroutine(waiter());
 
         }
-        if (counter > 2 && Bullets == 0)
+        if (counter > 1 && Bullets == 0)
         {
             animator.SetInteger("Reloading", 1);
             Bullets = 5;
@@ -56,15 +55,8 @@ public class Atirar : MonoBehaviour
         GameObject tiro = Instantiate(tiroPrefab , arma.position,arma.rotation);
         Rigidbody2D rb= tiro.GetComponent<Rigidbody2D>();
         rb.AddForce(-arma.up * bulletForce, ForceMode2D.Impulse);
-
-        StartCoroutine(waiter());
-    }
-    
-    IEnumerator waiter()
-    {
-        Debug.Log(Time.time);
-        yield return new WaitForSeconds(1f);
-        Debug.Log(Time.time);
+        AudioManager.PlaySFX(shootSFX);
+        
     }
 
 }
