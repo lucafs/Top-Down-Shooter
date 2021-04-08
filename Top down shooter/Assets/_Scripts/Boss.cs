b7ad3bfe23a810e9f8a4489b42865be1f782aae2
gubenites <gubenites99@gmail.com>
2021-04-08 11:52:27 -0300

    atualizado

---
+++ Top down shooter/Assets/_Scripts/Boss.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour
+{
+    GameManager gm;
+
+    public float velocidade;
+    public Rigidbody2D rb;
+    private Transform alvo;
+    public Vector2 positionZ;
+    public GameObject bloodObject;
+    Animator animator;
+
+    private int vidaBoss = 6;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        alvo = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        rb = GetComponent<Rigidbody2D>();
+        gm = GameManager.GetInstance();
+        animator = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gm.gameState != GameManager.GameState.GAME) return;
+
+        if (Vector2.Distance(transform.position, alvo.position) > 9)
+        {
+            Debug.Log(Vector2.Distance(transform.position, alvo.position));
+            animator.SetInteger("Atking", 0);
+            rb.MovePosition(transform.position - transform.up * velocidade * Time.deltaTime);
+            RotateTowards(alvo.position);
+        }
+        if(Vector2.Distance(transform.position, alvo.position) < 9)
+        {
+            animator.SetInteger("Atking", 1);
+            Debug.Log(animator.GetInteger("Atking"));
+            rb.MovePosition(transform.position - transform.up * velocidade * Time.deltaTime);
+            RotateTowards(alvo.position);
+        }
+    }
+
+    private void RotateTowards(Vector2 alvo)
+    {
+        if (gm.gameState != GameManager.GameState.GAME) return;
+
+        var offset = 90f;
+        Vector2 direction = alvo - (Vector2)transform.position;
+        direction.Normalize();
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(Vector3.forward * (angle + offset + 360));
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Bullet")
+        {
+            vidaBoss -= 1;
+            if (vidaBoss <= 0)
+            {
+                gm.pontos += 5;
+                Destroy(gameObject);
+
+                GameObject BloodInstance = Instantiate(bloodObject, transform.position, Quaternion.identity);
+            }
+        }
+    }
+
+}
--- Top down shooter/Assets/_Scripts/EnemySpawner.cs
+++ Top down shooter/Assets/_Scripts/EnemySpawner.cs
@@ -7,9 +7,12 @@ public class EnemySpawner : MonoBehaviour
     GameManager gm;
 
     public GameObject enemy;
+    public GameObject Boss;
     private int timeStamp;
     private float contadorSegundos = 0;
     private int difCount = 0;
+    private int countBoss = 0;
+    private int hordes = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -26,11 +29,23 @@ public class EnemySpawner : MonoBehaviour
     {
         for(int i = 0; i < loopCount; i++)
         {
-            float spawnYindex = Random.Range(Camera.main.ScreenToWorldPoint(new Vector2(0, 0)).y, Camera.main.ScreenToWorldPoint(new Vector2(0, Screen.height)).y);
-            float spawnXindex = Random.Range(Camera.main.ScreenToWorldPoint(new Vector2(0, 0)).x, Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, 0)).x);
+            float spawnYindex = Random.Range(new Vector2(0, -45).y, new Vector2(0, 2).y);
+            float spawnXindex = Random.Range(new Vector2(-90, 0).x, new Vector2(15, 0).x);
 
             Vector2 posicaoDeSpawn = new Vector2(spawnXindex + 10f, spawnYindex +10f);
             Instantiate(enemy, posicaoDeSpawn, Quaternion.identity);
+        }
+    }
+
+    void spawnBoss(int loopCount)
+    {
+        for (int i =0; i < loopCount; i++)
+        {
+            float spawnYindex = Random.Range(new Vector2(0, -45).y, new Vector2(0, 2).y);
+            float spawnXindex = Random.Range(new Vector2(-90, 0).x, new Vector2(15, 0).x);
+
+            Vector2 posicaoDeSpawn = new Vector2(spawnXindex + 10f, spawnYindex + 10f);
+            Instantiate(Boss, posicaoDeSpawn, Quaternion.identity);
         }
     }
 
@@ -51,20 +66,25 @@ public class EnemySpawner : MonoBehaviour
             {
                 difCount = 1;
                 timeStamp = 20;
+                hordes += 1;
                 spawnEnemies(10);
             }
             if (timeStamp == 0 && difCount == 1)
             {
                 difCount = 2;
                 timeStamp = 15;
+                hordes += 1;
                 spawnEnemies(15);
             }
             if (timeStamp == 0 && difCount == 2)
             {
+                difCount = 0;
                 timeStamp = 10;
+                hordes += 1;
+                countBoss += 1;
                 spawnEnemies(20);
+                spawnBoss(countBoss);
             }
-
             contadorSegundos = 0;
         }
 
