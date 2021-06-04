600c9091e8f3e0e7ded63432a10c758f6a305e97
Gustavo Molina Freneda Benites <gustavo.benites@onepartners.com.br>
2021-06-04 01:04:12 -0300

    atualizado

--- Top down shooter/Assets/_Scripts/EnemySpawner.cs
+++ Top down shooter/Assets/_Scripts/EnemySpawner.cs
@@ -23,6 +23,8 @@ public class EnemySpawner : MonoBehaviour
     private int spawnIndexRandom;
     private Vector3 myNewVector;
 
+    public int[] array_local;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -64,25 +66,54 @@ public class EnemySpawner : MonoBehaviour
 
         for (int i = 0; i < loopCount; i++)
         {
-            spawnIndexRandom = Random.Range(0, countFilhos);
-            myNewVector = new Vector3(i/5, i/5, i/5);
-            //spawnYindex = Random.Range(new Vector2(0, -45).y, new Vector2(0, 2).y);
-            //spawnXindex = Random.Range(new Vector2(-90, 0).x, new Vector2(15, 0).x);
+            if (gm.level_setter == 0)
+            {
+                spawnIndexRandom = Random.Range(0, 2);
+                myNewVector = new Vector3(i / 5, i / 5, i / 5);
+                
+                Instantiate(enemy, spawnpoints[spawnIndexRandom].position + myNewVector, Quaternion.identity);
+            }
+            
+            if (gm.level_setter == 1 && gm.position_doors.x > 25.0f && gm.position_doors.x < 35.0f && gm.position_doors.y > 8.0f && gm.position_doors.y < 13.0f)
+            {
+                array_local = new  int[] { 1, 2, 3, 4};
+       
+                spawnIndexRandom = Random.Range(0, 4);
 
-            //while(spawnXindex <= playerXpositionRight && spawnXindex >= playerXpositionLeft || spawnYindex >= playerYpositionDown && spawnYindex <= playerYpositionUp)
-            //{
-            //    spawnYindex = Random.Range(new Vector2(0, -45).y, new Vector2(0, 2).y);
-            //    spawnXindex = Random.Range(new Vector2(-90, 0).x, new Vector2(15, 0).x);
-            //}
+                myNewVector = new Vector3(i / 5, i / 5, i / 5);
 
-            //while (spawnYindex <= minorY && spawnYindex >= majorY && spawnXindex <= minorX && spawnXindex >= majorX)
-            //{
-            //    spawnYindex = Random.Range(new Vector2(0, -45).y, new Vector2(0, 2).y);
-            //    spawnXindex = Random.Range(new Vector2(-90, 0).x, new Vector2(15, 0).x);
-            //}
-            //posicaoDeSpawn = new Vector2(spawnXindex + 10f, spawnYindex + 10f);
-            Instantiate(enemy, spawnpoints[spawnIndexRandom].position + myNewVector, Quaternion.identity);
+                Instantiate(enemy, spawnpoints[spawnIndexRandom].position + myNewVector, Quaternion.identity);
 
+            }
+
+            if (gm.level_setter == 1 && gm.position_doors.x < -70.0f && gm.position_doors.x > -95.0f && gm.position_doors.y > 6.0f && gm.position_doors.y < 15.0f)
+            {
+                array_local = new int[] { 0, 1, 2, 5};
+                
+                spawnIndexRandom = Random.Range(0, 4);
+                myNewVector = new Vector3(i / 5, i / 5, i / 5);
+                spawnIndexRandom = array_local[spawnIndexRandom];
+
+                Instantiate(enemy, spawnpoints[spawnIndexRandom].position + myNewVector, Quaternion.identity);
+                
+            }
+
+            if (gm.level_setter == 1 && gm.position_doors.x < -70.0f && gm.position_doors.x > -95.0f && gm.position_doors.y > -50.0f && gm.position_doors.y < -20.0f)
+            {
+                array_local = new int[] { 0, 1, 2, 6, 7, 8 };
+
+                spawnIndexRandom = Random.Range(0, 6);
+                myNewVector = new Vector3(i / 5, i / 5, i / 5);
+                spawnIndexRandom = array_local[spawnIndexRandom];
+
+                Instantiate(enemy, spawnpoints[spawnIndexRandom].position + myNewVector, Quaternion.identity);
+
+            }
+
+            if (gm.level_setter == 2 && gm.position_doors.x > 25.0f && gm.position_doors.x < 35.0f && gm.position_doors.y > 8.0f && gm.position_doors.y < 13.0f)
+            {
+                
+            }
 
         }
     }
@@ -110,7 +141,7 @@ public class EnemySpawner : MonoBehaviour
         if (gm.gameState != GameManager.GameState.GAME) return;
         if (gm.reset == 1 )
         {
-            spawnEnemies(15);
+            spawnEnemies(50);
             timeStamp = 25;
             gm.reset++;
 
@@ -127,14 +158,14 @@ public class EnemySpawner : MonoBehaviour
                 gm.difCount = 1;
                 timeStamp = 20;
                 gm.hordes += 1;
-                spawnEnemies(10);
+                spawnEnemies(60);
             }
             if (timeStamp == 0 && gm.difCount == 1)
             {
                 gm.difCount = 2;
                 timeStamp = 15;
                 gm.hordes += 1;
-                spawnEnemies(15);
+                spawnEnemies(70);
             }
             if (timeStamp == 0 && gm.difCount == 2)
             {
@@ -142,7 +173,7 @@ public class EnemySpawner : MonoBehaviour
                 timeStamp = 10;
                 gm.hordes += 1;
                 countBoss += 1;
-                spawnEnemies(20);
+                spawnEnemies(80);
                 spawnBoss(countBoss);
             }
             contadorSegundos = 0;
